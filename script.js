9b534d13553b2ce7c54241d7b882702065927395
Prohalladdas <prolladdas@gmail.com>
2024-02-26 22:01:46 +0530

    This is zerodha frontend first code

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function  (){
+    window.addEventListener('scroll',function (e){
+        if(window.pageYOffset > 100){
+            document.querySelector("div").classList.add('is-scrolling');
+        }else{
+            document.querySelector("div").classList.remove('is-scrolling');
+        }
+    })
+
+    const menu_btn = document.querySelector('.humbergar');
+    const mobile_menu = document.querySelector('.mobile-nav');
+    const mobile_cont = document.querySelector('#mob-cont');
+
+    menu_btn.addEventListener('click',function(){ // iss-active wohi per  hi hoga jahaper click event hoga matlab hamburger and content ki container
+        menu_btn.classList.toggle('iss-active');
+        mobile_menu.classList.toggle('iss-active');
+        mobile_cont.classList.toggle('iss-active');
+    })
+}
